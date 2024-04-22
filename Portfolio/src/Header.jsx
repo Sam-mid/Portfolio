4795cbeb3e6e9beef5422f90e337603e868618e3
Sam Middendorp <sammiddendorp2002@gmail.com>
2024-04-22 14:36:27 +0200

    header aangepast

--- Portfolio/src/Header.jsx
+++ Portfolio/src/Header.jsx
@@ -19,7 +19,9 @@ const Header = ({ modelPath }) => {
 
         // Camera
         camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        camera.position.z = 80;
+        camera.position.z = 90;
+        camera.position.y = 1;
+        camera.position.x = -20;
 
         // Scene
         const scene = new THREE.Scene();
