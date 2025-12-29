777d92646e4e6300440464fd34cee380f92e9ad7
Sam Middendorp <113996114+Sam-mid@users.noreply.github.com>
2025-12-29 11:22:50 +0100

    Update App.jsx

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
                 <Header />
                   < Navbar />
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    < Route path="/" element={<Home />} />
                 </Routes>
                 <Footer /> 
             </div>
